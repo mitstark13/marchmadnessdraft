734d8030ae06df96fdd2a883668fd900e6e67be6
Starkey <mstarkey@MSTARKEY.local>
2019-03-08 15:45:19 -0500

    review page change to look/feel like draft history

--- src/components/review/Review.js
+++ src/components/review/Review.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import DraftTeams from '../draft/draftTeams/DraftTeams'
+import '../draft/draftHistory/DraftHistory.css';
 
 class Review extends Component {
   constructor(props) {
@@ -11,6 +11,19 @@ class Review extends Component {
       ownersList: [],
       owner: ''
     };
+  }
+  
+  componentDidMount() {
+
+    axios.get(this.props.dbUrl + '/players').then((players) => {
+      let draftOrder = players.data[0].owners
+      this.setState({ ownersList: draftOrder })
+      this.setState({ owner: draftOrder[0] })
+      this.setState({ players: players.data })
+    });
+
+    this.viewNewTeam = this.viewNewTeam.bind(this);
+    this.resetDraft = this.resetDraft.bind(this);
   }
 
   viewNewTeam() {
@@ -26,36 +39,49 @@ class Review extends Component {
       })
   }
 
-  componentDidMount() {
-
-    axios.get(this.props.dbUrl + '/players').then((players) => {
-      let draftOrder = players.data[0].owners
-      this.setState({ ownersList: draftOrder })
-      this.setState({ owner: draftOrder[0] })
-      this.setState({ players: players.data })
-    });
-
-    this.viewNewTeam = this.viewNewTeam.bind(this);
-    this.resetDraft = this.resetDraft.bind(this);
-  }
-
   render() {
 
-  	const draftedOrder = this.state.players
+    const draftedOrder = this.state.players
     draftedOrder.sort((a, b) => {
-      return Number(b.pickNumber) - Number(a.pickNumber)
+      return Number(a.pickNumber) - Number(b.pickNumber)
     })
 
     return (
-      <section id="draftReview">
+      <section className="draftHistory review">
         <h1> Draft Review </h1>
         <button onClick={this.resetDraft}> Reset Draft </button>
-        <DraftTeams
-            players={draftedOrder}
-            owner={this.state.owner}
-            viewNewTeam={this.viewNewTeam}
-            ownersList={this.state.ownersList}
-          />
+
+        <table>
+          <thead>
+            <tr>
+              <th className="pick">#</th>
+              <th className="name">Player</th>
+              <th className="team">Team</th>
+              <th className="owner">Owner</th>
+            </tr>
+          </thead>
+          <tbody>
+          {draftedOrder.map((player, i) => {
+            const drafted = player.pickNumber > 0
+
+            if (drafted) {
+              let name = player.name
+              name = name.length < 19 ? name : name.slice(0, 16) + '...'
+              
+              return (
+                <tr key={i}>
+                  <td className="pick">{player.pickNumber}</td>
+                  <td className="name">{name}</td>
+                  <td className="team">{player.team}</td>
+                  <td className="owner">{player.owner}</td>
+                </tr>
+              );
+            } else {
+              return false
+            }
+          })}
+          </tbody>
+        </table>
       </section>
     )
   }
