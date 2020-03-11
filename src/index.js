3084a961fcf34c2ea77d4a8d66808920c6b733c0
Mitchell <mstarkey@redventures.com>
2020-03-11 14:48:19 -0400

    prod dburl

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App dbUrl='http://192.168.0.9:5000'/>, document.getElementById('root'));
+ReactDOM.render(<App dbUrl='http://157.230.214.56:5000'/>, document.getElementById('root'));
 registerServiceWorker();
